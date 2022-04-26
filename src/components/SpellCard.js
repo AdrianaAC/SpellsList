1677513f3264623687b4fbeedcbbd382feb2e135
AdrianaAC <adrianaalves098@gmail.com>
2022-04-26 22:17:23 +0100

    List and details presenting. Setup for img, selectbox and reverse selection

--- src/components/SpellCard.js
+++ src/components/SpellCard.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function SpellCard(props) {
+	// const [img, setImg] = useState("../images/blue");
+	// if(props.spell.light === "Blue" || props.spell.light === "IcyBlue" || props.spell.light === "BrightBlue" || props.spell.light === "BlueishWhite" ) {
+	// 	setImg("../images/blue")
+	// }
 
   return (
     <div className="spell-card">
-      {/* <img/> */}
+      {/* <img src={img} alt="spell light"/> */}
       <h3>{props.spell.name}</h3>
       <div className="info-group">
 			  <p>Creator: {props.spell.creator}</p>
--- src/components/Spells.js
+++ src/components/Spells.js
@@ -2,42 +2,146 @@ import React, { useState } from "react";
 
 export default function Spells(props) {
   const [spells, setSpells] = useState([]);
+  const [spellType, setSpellType] = useState([])
+  const [checked, setChecked] = useState(false);
   // const [spellID, setSpellID] = useState();
-
+  // const [input, setInput] = useState("ola");
   // let spellName = [];
   // let spellNameJSX = [];
+
 
   const spellsListing = function getSpells() {
     fetch("https://wizard-world-api.herokuapp.com/Spells")
       .then((res) => res.json())
       .then((data) => {
         let spellsArr = [];
+        let spellsTp = [];
         // spellName = data.map(obj => obj.name)
         // //console.log(spellName)
         // return spellName
         for (let i = 0; i < data.length; i++) {
           spellsArr.push(data[i]);
-        }
-        //console.log(spellsArr)
-        setSpells(spellsArr);
-      });
+          
+          if(!spellsTp.includes(data[i].type)){
+            spellsTp.push(data[i].type)
+          }
+
+          setSpells(spellsArr);
+          setSpellType(spellsTp)
+        // console.log(input)
+      }
+      
+    })};
+    function spellsReverse() {
+    let spellsReverse = spells;
+    for (let i = spellsReverse.length - 1; i > 0; i -= 1) {
+      spellsReverse.shift();
+      spellsReverse.push(i);
+      
+    }
+    return spellsReverse;
+  }
 
     // for(let i = 0; i<spellName.length; i++) {
     //     spellNameJSX.push(<p key={i} className="item">{spellName[i]}</p>)
     //     console.log(spellNameJSX)
     // }
-  };
+  
 
   function handleClick(obj) {
-    // console.log(id)
+    console.log(spellType)
     props.setSpellID(obj.id);
     props.setSpell(obj)
+    
   }
+
+  function handleChange(){
+    setChecked(!checked)
+  }
+
+//   function SelectBox() {
+//     return(
+//     <div className="checkboxForm">
+
+//         <label className="labelCheckbox">
+//           All Types
+//         <input
+//           type="checkbox" id="charm" checked={checked}
+//           onChange={handleChange}
+//         />
+//       </label>
+
+//       <label className="labelCheckbox">
+//           Charm
+//         <input
+//           type="checkbox" id="charm" checked={checked}
+//           onChange={handleChange}
+//         />
+//       </label>
+
+//       <label className="labelCheckbox">
+//           Conjuration
+//         <input
+//           type="checkbox" id="conjuration" checked={checked}
+//           onChange={handleChange}
+//         />
+//       </label>
+
+//       <label className="labelCheckbox">
+//           Transfiguration
+//         <input
+//           type="checkbox" id="transfiguration" checked={checked}
+//           onChange={handleChange}
+//         />
+//       </label>
+
+//       <label className="labelCheckbox">
+//           Spell
+//         <input
+//           type="checkbox" id="spell" checked={checked}
+//           onChange={handleChange}
+//         />
+//       </label>
+
+//       <label className="labelCheckbox">
+//           Jinx
+//         <input
+//           type="checkbox" id="jinx" checked={checked}
+//           onChange={handleChange}
+//         />
+//       </label>
+
+//       <label className="labelCheckbox">
+//           Curse
+//         <input
+//           type="checkbox" id="curse" checked={checked}
+//           onChange={handleChange}
+//         />
+//       </label>
+
+//       <label className="labelCheckbox">
+//           Other
+//         <input
+//           type="checkbox" id="other" checked={checked}
+//           onChange={handleChange}
+//         />
+//       </label>
+
+
+//     </div>
+// );
+//   }
+  
 
   return (
     <div>
-      <input type="text"></input>
-      <button onClick={spellsListing}>Get spells!</button>
+      {/* <label>Search spell:</label>
+      <input type="text" value={input} onInput={e => setInput(e.target.value)}
+       placeholder="Name spell"></input> 
+       <SelectBox/>*/}
+       
+      <br/><button onClick={spellsListing}>Accio!</button>
+      {/* <br/><button onClick={spellsReverse}>Reverse Accio!</button> */}
       <div className="listBox">
         {spells.length > 0
           ? spells.map((obj) => (
